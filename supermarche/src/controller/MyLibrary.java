e2102be4e817aeb708e284afd0acb3bae31fa8e9
dsi <dsi@A0004160>
2021-12-29 15:27:53 +0100

    commit Mylibrairy test2

--- supermarche/src/controller/MyLibrary.java
+++ supermarche/src/controller/MyLibrary.java
@@ -1,5 +1,23 @@
 package controller;
 
+import javax.swing.JOptionPane;
+
 public class MyLibrary {
 
+	public static void afficher(String msg) {
+		JOptionPane.showMessageDialog(null, msg);
+	}
+	
+	public static String stringSaisie(String msg) {
+		return JOptionPane.showInputDialog(msg);
+	}
+	
+	public static int intSaisie(String msg) {
+		return Integer.parseInt(JOptionPane.showInputDialog(msg)); 
+	}
+	
+	public static double doubleSaisie(String msg) {
+		return Double.parseDouble(JOptionPane.showInputDialog(msg)); 
+	}
+	
 }
