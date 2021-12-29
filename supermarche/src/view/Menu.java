b976c19912b29f4b5b79b75c8c103a8eb96c741c
dsi <dsi@A0003812>
2021-12-29 16:10:19 +0100

    test

--- supermarche/src/view/Menu.java
+++ supermarche/src/view/Menu.java
@@ -4,26 +4,48 @@ import controller.MyLibrary;
 
 public class Menu {
 
-	private static String MSG_MENU_UTILISATEUR = "Menu Utilisateur";
 	
-	public static void menuUtilisateur() {
+	/* 
+------------------------------------------
+				Menu Admin
+------------------------------------------
+	*/
+	
+	private static String MSG_MENU_PERSON = "Menu Admin \n\n\n Bienvenue administrateur. \n\n\n 1 - Création d'une carte \n 2 - Détails d'une carte utilisateur \n 3 - Gestions des produits \n 4 - Menu de la cantine \n 5 - Désactivation d'une carte \n 6 - Activation d'une carte \n 7 - Ajouter une promotion sur un produit \n\n\n 0 - Quitter";
+	
+	public static void Admin () {
 		boolean sortie = false;
 		while (!sortie) {
-			switch (MyLibrary.intSaisie(MSG_MENU_UTILISATEUR)) {
+			switch(MyLibrary.intSaisie(MSG_MENU_PERSON)) {
 			case 1 :
-				
+				//Création d'une carte 
 				break;
 			case 2 :
-				//menuPerson();
+				//Détails d'une carte utilisateur
+				break;
+			case 3 :
+				//Gestions des produits
+				break;
+			case 4 :
+				//Menu de la cantine
+				break;
+			case 5 :
+				//Désactivation d'une carte
+				break;
+			case 6 :
+				//Activation d'une carte
+				break;
+			case 7 :
+				//Ajouter une promotion sur un produit
 				break;
 			case 0 :
 				sortie = true;
 				break;
+				
 			default:
-				MyLibrary.afficher("erreur");
+				MyLibrary.afficher("Erreur");
 				break;
 			}
 		}
 	}
-	
 }
