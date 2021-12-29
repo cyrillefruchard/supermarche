4b07a691e32be9c4b16c14f8f32fc06330f3d37c
dsi <dsi@A0004160>
2021-12-29 16:16:10 +0100

    commit Jonathan test

--- supermarche/src/modele/compte.java
+++ supermarche/src/modele/compte.java
@@ -4,4 +4,44 @@ public class compte {
 	private String num;
 	private double solde;
 	private Utilisateur titulaire;
+	
+	public compte(String num, double solde, Utilisateur titulaire) {
+		
+		this.num = num;
+		this.solde = solde;
+		this.titulaire = titulaire;
+	}
+
+	public compte() {
+		
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	public Utilisateur getTitulaire() {
+		return titulaire;
+	}
+
+	public void setTitulaire(Utilisateur titulaire) {
+		this.titulaire = titulaire;
+	}
+	
+	
+	
+	
+	
 }
