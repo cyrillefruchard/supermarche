7bd957f209e3c8c65f5f1df762bf4104468c2f92
dsi <dsi@A0004160>
2021-12-30 09:33:03 +0100

    update compte : supprime num

--- supermarche/src/modele/compte.java
+++ supermarche/src/modele/compte.java
@@ -1,28 +1,22 @@
 package modele;
 
 public class compte {
-	private String num;
 	private double solde;
 	private Utilisateur titulaire;
 	
-	public compte(String num, double solde, Utilisateur titulaire) {
+	
+
+	public compte(double solde, Utilisateur titulaire) {
 		
-		this.num = num;
 		this.solde = solde;
 		this.titulaire = titulaire;
 	}
+
 
 	public compte() {
 		
 	}
 
-	public String getNum() {
-		return num;
-	}
-
-	public void setNum(String num) {
-		this.num = num;
-	}
 
 	public double getSolde() {
 		return solde;
@@ -41,9 +35,7 @@ public class compte {
 	}
 	
 	
-	public String infoCompte() {
-		return "votre solde : " + getSolde();
-	}
+	
 	
 	
 }
