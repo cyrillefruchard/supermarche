97753089f70b8b14a4e19b0777da6e6ddae001a8
dsi <dsi@A0004130>
2021-12-30 13:58:37 +0100

    rdvmedecin

---
+++ supermarche/src/modele/rdvmedecin.java
@@ -0,0 +1,65 @@
+package modele;
+
+public class rdvmedecin {
+
+	private String medecin;
+	private String patient;
+	private String date;
+	private String heuredebut;
+	private String heurefin;
+	private double prix;
+	
+	
+	
+	public rdvmedecin() {
+	}
+	
+	public rdvmedecin(String medecin, String patient, String date, String heuredebut, String heurefin, double prix) {
+		this.medecin = medecin;
+		this.patient = patient;
+		this.date = date;
+		this.heuredebut = heuredebut;
+		this.heurefin = heurefin;
+		this.prix = prix;
+	}
+	
+	public String getMedecin() {
+		return medecin;
+	}
+	public void setMedecin(String medecin) {
+		this.medecin = medecin;
+	}
+	public String getPatient() {
+		return patient;
+	}
+	public void setPatient(String patient) {
+		this.patient = patient;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getHeuredebut() {
+		return heuredebut;
+	}
+	public void setHeuredebut(String heuredebut) {
+		this.heuredebut = heuredebut;
+	}
+	public String getHeurefin() {
+		return heurefin;
+	}
+	public void setHeurefin(String heurefin) {
+		this.heurefin = heurefin;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	
+	
+	
+}
